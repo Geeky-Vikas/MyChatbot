6a7823206306eb9d39ad1a5bd47a219c6ed329bf
Vikas Babu (Personal) <85045161+Geeky-Vikas@users.noreply.github.com>
2023-10-09 13:33:58 +0530

    Update app.py
    h5 named ki updated and Intensts updated

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import pickle
 import numpy as np
 
 from keras.models import load_model
-model = load_model('Chatbot_model.h5')
+model = load_model('trained_model.h5')
 import json
 import random
-intents = json.loads(open('intents.json').read())
+intents = json.loads(open('Intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
 
